b38515bcd79874f1d51c6699552a9663b6428aee
anhedonix <anhedonix@gmail.com>
2017-02-09 16:23:35 +0530

    Added base values
    Added categorization, HSV values for sorting (has to be implemented
    yet), added Ch values that have to be integrated with RGB yet

--- color_library/admin.py
+++ color_library/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Color
+from .models import Color, MainCategory, Category
 
 admin.site.register(Color)
+admin.site.register(MainCategory)
+admin.site.register(Category)
